10f298d18249ca40a90fca8152b4e5215580cb3b
rakanishu37d <84699618+rakanishu37d@users.noreply.github.com>
2021-05-26 10:22:26 +0800

    Revert "simple"
    This reverts commit 9967175b04660377fe3f293c956184b3132b828b.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,13 @@
 /*!
-* Start Bootstrap - Grayscale v7.0.0 (https://startbootstrap.com/theme/grayscale)
+* Start Bootstrap - Scrolling Nav v5.0.0 (https://startbootstrap.com/template/scrolling-nav)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
 */
 //
 // Scripts
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
-
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
 
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
